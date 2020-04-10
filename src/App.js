d91fa93ca5f9b0283aa351a27c17a26ddc5e064a
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-04-09 22:44:11 -0700

    built starting file structure, added dependencies for charts, counting up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Cards, Charts, CountryPicker } from './components';
+import styles from './App.module.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <Charts />
+        <CountryPicker />
+      </div>
+    );
+  }
+}
+
+export default App;
