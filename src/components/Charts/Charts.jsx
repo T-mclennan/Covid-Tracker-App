036076e6eef1fdb9d2eb0bcb43dc92f43bc24060
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2020-04-13 16:20:03 -0700

    added Bar chart to display data for selected country

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from 'react-chartjs-2';
 
 import styles from './Charts.module.css';
 
-const Charts = () => {
+const Charts = ({ data: { confirmed, deaths, recovered }, country }) => {
   const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
@@ -14,7 +14,7 @@ const Charts = () => {
     console.log(dailyData);
 
     fetchAPI();
-  }, [setDailyData]);
+  }, []);
 
   const lineChart = dailyData.length ? (
     <Line
@@ -39,7 +39,34 @@ const Charts = () => {
     />
   ) : null;
 
-  return <div className={styles.container}>{lineChart}</div>;
+  console.log(confirmed, recovered, deaths);
+
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ['Infected', 'Recovered', 'Deaths'],
+        datasets: [
+          {
+            label: 'People',
+            backgroundColor: [
+              'rgba(1, 1, 255, 0.5)',
+              'rgba(0, 255, 0, 0.5)',
+              'rgba(255, 0, 0, 0.5)',
+            ],
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Current state in ${country}` },
+      }}
+    />
+  ) : null;
+
+  return (
+    <div className={styles.container}>{country ? barChart : lineChart}</div>
+  );
 };
 
 export default Charts;
